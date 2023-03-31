856353e03eb89c223c642f037040550101a3a5e5
Ryan Samford <rsamfordnet@gmail.com>
2023-03-31 15:05:42 +0200

    Left more hints in the initial test

--- GildedRose.Core.Specs/GildedRoseSpecs.cs
+++ GildedRose.Core.Specs/GildedRoseSpecs.cs
@@ -8,9 +8,12 @@ namespace GildedRose.Core.Specs
 {
     public class GildedRoseSpecs
     {
-//        [Fact]
-//       public void Test()
-//        {
-//        }
+        [Fact]
+        public void Test()
+        {
+            List<Item> items = new GildedRose().MakeItems();
+            
+            
+        }
     }
 }
